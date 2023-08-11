cd8a5ce8e463263b6f1d5bc01ce4e45e1e54f1d7
mayank4code <119710987+mayank4code@users.noreply.github.com>
2023-08-11 12:21:47 +0530

    language on Instruction Page

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -5,7 +5,34 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast, } from "react-hot-toast";
 import "../css/instructions.css"
 
+
+//IMPORTS FOR Language change Functionality
+import i18n from "i18next";
+import { useTranslation } from "react-i18next";
+// import '../../library/i18n';
+
 function InstructionsPage() {
+
+
+//? Language Functionality Starts ............................................................
+  
+const { t } = useTranslation("translation", { keyPrefix: 'instruction' } );
+  
+
+//used to get language Stored in LocalStorage //*should be in every Page having Language Functionality 
+useEffect(()=>{
+  let currentLang = localStorage.getItem('lang');
+  i18n.changeLanguage(currentLang);
+
+  // console.log(t('array'  , { returnObjects: true }));
+
+},[]);
+
+
+//? Language Functionality Ends .................................................................
+
+
+
     const navigate = useNavigate();
     const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
 
@@ -53,45 +80,38 @@ function InstructionsPage() {
   return (
     <div className="instructions-page">
     {!isUserAuthenticated?"":(
-        <>
-        <h1>Test Instructions</h1>
-        <p>
-            Welcome to the test! This test contains 26 questions, and for each question, you'll have four options to choose from.
-        </p>
-        <p>
-            <strong>Instructions:</strong>
-        </p>
-        <ol>
-            <li>Ensure you are in a quiet environment to concentrate on the test.</li>
-            <li>Read each question carefully and choose the best option.</li>
-            <li>Once you select an option, you can also change it.</li>
-            <li>There is no time limit, so take your time and do your best!</li>
-            <li>After clicking the "Start Test" button, full-screen mode will be enabled.</li>
-            <li>This test is designed to assess your knowledge and skills in various areas.</li>
-            <li> Please answer each question to the best of your ability.</li>
-        </ol>
-        <p>
-            <strong>Note:</strong> If you accidentally exit full-screen mode during the test, you can click the "Go Full Screen" button again to re-enable it.
-        </p>
-        <div className="start-button-container">
-        <button onClick={()=>{
-                navigate('/test/start')
-                const element = document.documentElement;
-            if (element.requestFullscreen) {
+      <>
+          <h1>{t('testInstructions')}</h1>
+          <p>{t('welcomeMessage')}</p>
+          <p>
+            <strong>{t('instructionsHeader')}</strong>
+          </p>
+          <ol>
+            {t('instructionList', { returnObjects: true }).map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+          <p>
+            <strong>{t('note')}</strong>: {t('noteMessage')}
+          </p>
+          <div className="start-button-container">
+            <button onClick={() => {
+              navigate('/test/start');
+              const element = document.documentElement;
+              if (element.requestFullscreen) {
                 element.requestFullscreen();
-            } else if (element.mozRequestFullScreen) {
+              } else if (element.mozRequestFullScreen) {
                 // Firefox
                 element.mozRequestFullScreen();
-            } else if (element.webkitRequestFullscreen) {
+              } else if (element.webkitRequestFullscreen) {
                 // Chrome, Safari, and Opera
                 element.webkitRequestFullscreen();
-            } else if (element.msRequestFullscreen) {
+              } else if (element.msRequestFullscreen) {
                 // Internet Explorer and Edge
                 element.msRequestFullscreen();
-            }
-            }} className='btn btn-primary'> Start Test</button>
-        </div>
-
+              }
+            }} className='btn btn-primary'>{t('startTestButton')}</button>
+          </div>
         </>
     )}
     </div>
