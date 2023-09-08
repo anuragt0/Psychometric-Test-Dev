f51c6d0f4def97aaa2f1e1bd7314fe9755aa0f77
mayank4code <119710987+mayank4code@users.noreply.github.com>
2023-09-08 20:38:32 +0530

    marathi added 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React , {useEffect } from 'react';
+import React from 'react';
 import './App.css'
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -6,7 +6,7 @@ import Navbar from './components/NavBar';
 import Home from "../src/components/neha/Home";
 import Login from './components/Login';
 import Register from './components/Register';
-import Result_M from './components/mayank/Result_M';
+import Result from './components/mayank/Result_M';
 import Congrats from './components/Congrats.js';
 import About from './components/About.js';
 
@@ -32,7 +32,7 @@ function App() {
           <Route path="/test/start" element={<Quiz/>} />
           <Route path="/register" element={<Register/>} />
           <Route path="/login" element={<Login/>} />
-          <Route path="/test/result" element={<Result_M/>} />
+          <Route path="/test/result" element={<Result/>} />
           <Route path="/test/submit" element={<Congrats/>} />
           <Route path="/about" element={<About/>} />
         
